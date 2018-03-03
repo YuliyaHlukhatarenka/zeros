0b0b58389fc04646f2d61fc04b9a8f8bd1514879
Dmitry <d.gluhotorenko@gmail.com>
2018-03-03 20:25:16 +0300

    'zeros/feature:implement'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var fiveDegree = 1;
+  var result = 0;
+  while (Math.floor(number/(5*fiveDegree)>0)) {
+    result = result + Math.floor(number/(5*fiveDegree));
+    fiveDegree = fiveDegree*5;
+    }
+    return result;
 }
